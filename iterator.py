a375c22b3a412d8f4b448336ed6a2578e56fced1
Wang Hongwei <525269029@qq.com>
2018-07-24 16:23:30 +0800

    从pycharm中提交

---
+++ iterator.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+'''
+    练习迭代器的使用及编写
+'''
+def frange(start, stop, step):
+    x = start
+    while x < stop:
+        yield x
+        x += step
+g = frange(0,2,0.4)
+print(g)
+print(next(g))
+
+def gen_infinet():
+    x = 0
+    while True:
+        yield x
+        x += 1
+
+g2 = gen_infinet()
+
+for i in g2:
+    print(i)
+    if i > 5:
+        break
